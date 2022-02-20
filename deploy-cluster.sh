47255cdc615fbcc949ce5ef28deb2333f417ba81
dawiddc <dawid.ciok@gmail.com>
2022-02-20 03:31:04 +0100

    Add RabbitMQ to cluster, move LICENSE, MQ Consumer fixes.

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -6,7 +6,9 @@ set -euo pipefail
 kubectl apply -f sample-cluster-namespace.yml
 
 # Spawn MQ instance
-# TODO: Create RabbitMQ instance
+kubectl apply -f rabbitmq/rabbitmq-deployment.yml
 
 # Spawn Spring Boot app
 kubectl apply -f will-change-later/will-change-later-deployment.yml
+
+# TODO: Add --rebuild option to rebuild nested images and apply changes
--- will-change-later/src/main/java/org/dawiddc/willchangelater/jms/SampleMqConsumer.java
+++ will-change-later/src/main/java/org/dawiddc/willchangelater/jms/SampleMqConsumer.java
@@ -3,14 +3,13 @@ package org.dawiddc.willchangelater.jms;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Optional.ofNullable;
 
-@Service
+@RestController
 @RequiredArgsConstructor
 public class SampleMqConsumer {
 
@@ -19,8 +18,10 @@ public class SampleMqConsumer {
     @GetMapping("/receiveMessage")
     public String getSampleFromQueue() {
         Message message = rabbitTemplate.receive("sampleQueue");
-        return ofNullable(message).map(Objects::toString).orElse("");
+        return ofNullable(message)
+                .map(Message::getBody)
+                .map(bytes -> new String(bytes, UTF_8))
+                .orElse("[NO_BODY_AVAILABLE]");
     }
-
 
 }
